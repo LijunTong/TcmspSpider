5a7207e6dabe14aa630149d4a6efb7453ac0e1a7
joont <1193247828@qq.com>
2023-07-28 01:28:09 +0800

    feat：完成基础版

---
+++ src/Tcmsp/Ingredients.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tcmsp
+{
+    public class Ingredients
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string molecule_ID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string MOL_ID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string file_ID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string molecule_name { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public decimal ob { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string mw { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string alogp { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string caco2 { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string bbb { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string halflife { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string hdon { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string hacc { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public decimal dl { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string FASA { get; set; }
+    }
+}
--- src/Tcmsp/MainFrm.cs
+++ src/Tcmsp/MainFrm.cs
@@ -1,6 +1,8 @@
 using HtmlAgilityPack;
 using Jt.Common.Tool.Extension;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
 using HtmlDocument = HtmlAgilityPack.HtmlDocument;
 
@@ -9,6 +11,9 @@ namespace Tcmsp
     public partial class MainFrm : Form
     {
         HttpClient httpClient;
+        private const string pattern1 = "(?<=\"herb_en_name\":\").*?(?=\",)";
+        private const string pattern2 = "(?<=data: ).*?(?=}],)";
+
 
         public MainFrm()
         {
@@ -18,20 +23,55 @@ namespace Tcmsp
 
         private void btnGet_Click(object sender, EventArgs e)
         {
-            if (this.tbToken.Text.IsNullOrWhiteSpace())
+            try
             {
-                MessageBox.Show("请填写token");
-                return;
-            }
+                if (this.tbToken.Text.IsNullOrWhiteSpace())
+                {
+                    MessageBox.Show("请填写token");
+                    return;
+                }
 
-            if (this.tbName.Text.IsNullOrWhiteSpace())
+                if (this.tbName.Text.IsNullOrWhiteSpace())
+                {
+                    MessageBox.Show("请填写名称");
+                    return;
+                }
+
+                if (!decimal.TryParse(this.tbObVal.Text, out decimal obval))
+                {
+                    MessageBox.Show("Ob值不对");
+                    return;
+                }
+
+                if (!decimal.TryParse(this.tbDlVal.Text, out decimal dlval))
+                {
+                    MessageBox.Show("DL值不对");
+                    return;
+                }
+                this.Cursor = Cursors.WaitCursor;
+
+                var enName = GetEnName(this.tbName.Text, this.tbToken.Text);
+                var data = GetTargets(enName, this.tbToken.Text);
+                var ings = data.Ingredients.Where(x => x.ob >= obval && x.dl >= dlval).ToList();
+                var target = data.RelatedTargets.Where(x => ings.Any(o => o.molecule_ID == x.molecule_ID)).ToList();
+                this.dgvIng.DataSource = ings;
+                this.dgvTarget.DataSource = target;
+                this.tssIng.Text = ings.Count.ToString();
+                this.tssTarget.Text = target.Count.ToString();
+            }
+            catch (Exception ex)
             {
-                MessageBox.Show("请填写名称");
-                return;
+                MessageBox.Show(ex.ToString());
             }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+            }
+        }
 
-            // string url = $"https://old.tcmsp-e.com/tcmspsearch.php?qs=herb_all_name&q={this.tbName.Text}&token={this.tbToken.Text}";
-            string url = $"https://old.tcmsp-e.com/tcmspsearch.php?qr={this.tbName.Text}&qsr=herb_en_name&token={this.tbToken.Text}";
+        private string GetEnName(string name, string token)
+        {
+            string url = $"https://old.tcmsp-e.com/tcmspsearch.php?qs=herb_all_name&q={name}&token={token}";
 
             // 初始化网络请求客户端
             HtmlWeb webClient = new HtmlWeb();
@@ -44,14 +84,50 @@ namespace Tcmsp
                 foreach (var item in titleNodes)
                 {
                     var subNodes = item.SelectNodes("script");
-                    foreach (var subItem in subNodes)
+                    if (subNodes != null && subNodes.Count > 0)
                     {
-                        Console.WriteLine(subItem.InnerHtml);
+                        var text = subNodes[0].InnerHtml;
+                        Regex regex = new Regex(pattern1);
+                        var matchs = regex.Matches(text);
+                        foreach (var match in matchs)
+                        {
+                            Console.WriteLine(match.ToString());
+                            return match.ToString();
+                        }
                     }
                 }
             }
+            return "";
+        }
 
-
+        private (List<Ingredients> Ingredients, List<RelatedTargets> RelatedTargets) GetTargets(string enName, string token)
+        {
+            string url = $"https://old.tcmsp-e.com/tcmspsearch.php?qr={enName}&qsr=herb_en_name&token={token}";
+            // 初始化网络请求客户端
+            HtmlWeb webClient = new HtmlWeb();
+            //初始化文档
+            HtmlDocument doc = webClient.Load(url);
+            //查找节点
+            HtmlNodeCollection titleNodes = doc.DocumentNode.SelectNodes("//div[@id='tabstrip']");
+            if (titleNodes != null)
+            {
+                foreach (var item in titleNodes)
+                {
+                    var subNodes = item.SelectNodes("script");
+                    if (subNodes != null && subNodes.Count > 0)
+                    {
+                        var text = subNodes[1].InnerHtml;
+                        Regex regex = new Regex(pattern2);
+                        var matchs = regex.Matches(text);
+                        string ingredientsJson = matchs[0].Value + "}]";
+                        List<Ingredients> ingredients = ingredientsJson.ToObj<List<Ingredients>>();
+                        string relatedTargetsJson = matchs[1].Value + "}]";
+                        List<RelatedTargets> relatedTargets = relatedTargetsJson.ToObj<List<RelatedTargets>>();
+                        return (ingredients, relatedTargets);
+                    }
+                }
+            }
+            return (null, null);
         }
     }
 }
