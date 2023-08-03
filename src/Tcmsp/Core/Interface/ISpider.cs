9262d0019bccbcac4051e6358a54f22e27650be6
WSY1227 <1669228230@qq.com>
2023-08-03 09:38:12 +0800

    fix:兼容

--- src/Tcmsp/Core/Interface/ISpider.cs
+++ src/Tcmsp/Core/Interface/ISpider.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tcmsp.Domian.SpiderDomain;
+using Tcmsp.Domain.SpiderDomain;
 
 namespace Tcmsp.Core.Interface
 {
