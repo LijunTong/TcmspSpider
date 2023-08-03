eeb03f7093291ca4f297cdce7cd682e30e26b8ad
WSY1227 <1669228230@qq.com>
2023-08-03 15:52:20 +0800

    fix:全局化配置文件

--- src/Tcmsp/Core/Impl/TcmspPlaywright.cs
+++ src/Tcmsp/Core/Impl/TcmspPlaywright.cs
@@ -1,99 +1,78 @@
 ﻿using Microsoft.Playwright;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Tcmsp.Core.Interface;
 using Tcmsp.Domain.SpiderDomain;
+using static Tcmsp.Program;
 
-namespace Tcmsp.Core.Impl
+namespace Tcmsp.Core.Impl;
+
+public class TcmspPlaywright : ISpider
 {
-    public class TcmspPlaywright : ISpider
+    private static IBrowserContext _context;
+    private static readonly object LockObject = new();
+
+    public TcmspPlaywright()
+   {
+       if (_context != null) return;
+       lock (LockObject)
+       {
+           if (_context != null) return;
+           var playwright = Playwright.CreateAsync().Result;
+           var browser = playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+           {
+               Headless = true,
+               ExecutablePath = ConfigJson["edgePath"]?.ToString()
+           }).Result;
+           _context = browser.NewContextAsync(new BrowserNewContextOptions
+           {
+               IgnoreHTTPSErrors = true
+           }).Result;
+       }
+   }
+    
+    private async Task<(List<Ingredients>, List<RelatedTargets>)> GetIngredientsAndRelatedTargets(string name)
     {
-        private static IBrowserContext _context;
-        private static readonly object LockObject = new();
-
-        private static readonly string ConfigFilePath = Path.Combine(Application.StartupPath, "Config.json");
-        private static JObject ConfigJson;
-
-        static TcmspPlaywright()
+       
+        var page = await _context.NewPageAsync();
+        await page.GotoAsync(
+            $"https://old.tcmsp-e.com/tcmspsearch.php?qs=herb_all_name&q={name}&token={ConfigJson["token"]}");
+        // 获取#kendoResult标签的文字是否包含Error querying database...
+        var errorText = await page.TextContentAsync("#kendoResult");
+        if (errorText != null && errorText.Contains("Error"))
         {
-            LoadConfig();
+            var tokenTable = "input[name='token'][type='hidden']";
+            var tokenElementHandle = await page.QuerySelectorAsync(tokenTable);
+            var token = await tokenElementHandle.GetAttributeAsync("value");
+            ConfigJson["token"] = token;
+            await File.WriteAllTextAsync(ConfigFilePath, ConfigJson.ToString());
+            await page.GotoAsync(
+                $"https://old.tcmsp-e.com/tcmspsearch.php?qs=herb_all_name&q={name}&token={ConfigJson["token"]}");
+            await page.WaitForLoadStateAsync(LoadState.DOMContentLoaded,
+                new PageWaitForLoadStateOptions { Timeout = 60000 });
         }
 
-        private static void LoadConfig()
-        {
-            ConfigJson = JObject.Parse(File.ReadAllText(ConfigFilePath));
-        }
+        // 点击链接
+        await page.ClickAsync("td[role=gridcell]:nth-child(3) a");
 
-        public TcmspPlaywright()
-        {
-            if (_context != null) return;
-            lock (LockObject)
-            {
-                if (_context == null)
-                {
-                    InitializeContextAsync().GetAwaiter().GetResult();
-                }
-            }
-        }
+        // 等待页面跳转完成，指定加载状态为"domcontentloaded"，增加等待时间为60秒
+        await page.WaitForLoadStateAsync(LoadState.DOMContentLoaded,
+            new PageWaitForLoadStateOptions { Timeout = 60000 });
+        // 获取dataSource数据
+        var ingredientJson =
+            await page.EvaluateAsync<string>("JSON.stringify($('#grid').data('kendoGrid').dataSource.data())");
+        var ingredients = JsonConvert.DeserializeObject<List<Ingredients>>(ingredientJson);
+        var relatedTargetJson =
+            await page.EvaluateAsync<string>("JSON.stringify($('#grid2').data('kendoGrid').dataSource.data())");
+        var relatedTargets = JsonConvert.DeserializeObject<List<RelatedTargets>>(relatedTargetJson);
+        return (ingredients, relatedTargets);
+    }
 
-        private async Task InitializeContextAsync()
-        {
-            var playwright = await Playwright.CreateAsync();
-            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-            {
-                Headless = true,
-                ExecutablePath = ConfigJson["edgePath"]?.ToString()
-            });
-            _context = await browser.NewContextAsync(new BrowserNewContextOptions
-            {
-                IgnoreHTTPSErrors = true
-            });
-        }
-
-        public async Task<(List<Ingredients>, List<RelatedTargets>)> GetIngredientsAndRelatedTargets(string name)
-        {
-            var page = await _context.NewPageAsync();
-            await page.GotoAsync($"https://old.tcmsp-e.com/tcmspsearch.php?qs=herb_all_name&q={name}&token={ConfigJson["token"]}");
-            
-            var errorText = await page.TextContentAsync("#kendoResult");
-            if (errorText != null && errorText.Contains("Error"))
-            {
-                var tokenTable = "input[name='token'][type='hidden']";
-                var tokenElementHandle = await page.QuerySelectorAsync(tokenTable);
-                var token = await tokenElementHandle.GetAttributeAsync("value");
-                ConfigJson["token"] = token;
-                await File.WriteAllTextAsync(ConfigFilePath, ConfigJson.ToString());
-                await page.GotoAsync($"https://old.tcmsp-e.com/tcmspsearch.php?qs=herb_all_name&q={name}&token={ConfigJson["token"]}");
-                await page.WaitForLoadStateAsync(LoadState.DOMContentLoaded, new PageWaitForLoadStateOptions { Timeout = 60000 });
-            }
-
-            await page.ClickAsync("td[role=gridcell]:nth-child(3) a");
-
-            await page.WaitForLoadStateAsync(LoadState.DOMContentLoaded, new PageWaitForLoadStateOptions { Timeout = 60000 });
-
-            var ingredientJson = await page.EvaluateAsync<string>("JSON.stringify($('#grid').data('kendoGrid').dataSource.data())");
-            var ingredients = JsonConvert.DeserializeObject<List<Ingredients>>(ingredientJson);
-            var relatedTargetJson = await page.EvaluateAsync<string>("JSON.stringify($('#grid2').data('kendoGrid').dataSource.data())");
-            var relatedTargets = JsonConvert.DeserializeObject<List<RelatedTargets>>(relatedTargetJson);
-            
-            return (ingredients, relatedTargets);
-        }
-
-        public async Task<(List<Ingredients>, List<RelatedTargets>)> GetIngredientsAndTargets(string name, decimal ob, decimal dl, string token = "")
-        {
-            var (ingredientsList, relatedTargetsList) = await GetIngredientsAndRelatedTargets(name);
-            var ings = ingredientsList.Where(x => x.Ob >= ob && x.Dl >= dl).ToList();
-            var target = relatedTargetsList.Where(x => ings.Any(o => o.MoleculeID == x.MoleculeID)).ToList();
-            return (ings, target);
-        }
-
-        public static async Task DisposeAsync()
-        {
-            if (_context != null)
-            {
-                await _context.CloseAsync();
-                _context = null;
-            }
-        }
+    public async Task<(List<Ingredients> Ingredients, List<RelatedTargets> RelatedTargets)> GetIngredientsAndTargets(
+        string name, decimal ob, decimal dl, string token = "")
+    {
+        var (ingredientsList, relatedTargetsList) = await GetIngredientsAndRelatedTargets(name); // 使用await来等待异步方法完成
+        var ings = ingredientsList.Where(x => x.Ob >= ob && x.Dl >= dl).ToList();
+        var target = relatedTargetsList.Where(x => ings.Any(o => o.MoleculeID == x.MoleculeID)).ToList();
+        return (ings, target);
     }
 }
